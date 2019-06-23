4fd3096244cc16fdfd092663b907e9295b7cb770
shinancao <shinancao666@163.com>
2019-06-23 21:15:15 +0800

    show UITableViewCell layout

--- Xib2ObjC_Client/Xib2ObjC_Client/XibLayoutViewController.m
+++ Xib2ObjC_Client/Xib2ObjC_Client/XibLayoutViewController.m
@@ -7,11 +7,8 @@
 //
 
 #import "XibLayoutViewController.h"
-#import "StreamHelper.h"
 
-@interface XibLayoutViewController ()
-
-@property (nonatomic, strong) StreamHelper *streamHelper;
+@interface XibLayoutViewController ()<UITableViewDelegate, UITableViewDataSource>
 
 @end
 
@@ -24,12 +21,16 @@
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Generate Code" style:UIBarButtonItemStylePlain target:self action:@selector(generateCodeAction:)];
     
     [self configView];
-    
-    self.streamHelper = [[StreamHelper alloc] init];
 }
 
 - (void)configView {
     UIView *containerView = [[[NSBundle mainBundle] loadNibNamed:self.xibName owner:self options:nil] lastObject];
+    
+    if ([containerView isKindOfClass:[UITableViewCell class]]) {
+        [self configTableView];
+        return;
+    }
+    
     [self.view addSubview:containerView];
     [containerView setTranslatesAutoresizingMaskIntoConstraints:NO];
     
@@ -47,8 +48,41 @@
     
 }
 
+- (void)configTableView {
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    [self.view addSubview:tableView];
+    [tableView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(tableView);
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[tableView]-0-|" options:0 metrics:nil views:viewsDictionary]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[tableView]-|" options:0 metrics:nil views:viewsDictionary]];
+    
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    
+    [tableView registerNib:[UINib nibWithNibName:self.xibName bundle:[NSBundle mainBundle]] forCellReuseIdentifier:@"tableCell"];
+}
+
 - (void)generateCodeAction:(id)sender {
-    NSLog(@"generateCodeAction");
+    NSString *cmd = [NSString stringWithFormat:@"parse:%@", self.xibName];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"com.xib2objc.sendMsg" object:cmd];
+}
+
+#pragma mark - Table view data source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"tableCell" forIndexPath:indexPath];
+    return cell;
 }
 
 @end
